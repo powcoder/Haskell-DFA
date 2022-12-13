073ddc8a056f35511caa7debe80a74dd4afe6a8f
powcoder <powcoder@163.com>
2022-12-13 21:38:59 +0800

    Haskell DFA

---
+++ Main.hs
@@ -0,0 +1,20 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+-- we provide the same files as in assessed worksheet 4
+import DFA
+import NFA
+import VisDFA
+-- we provide solutions to worksheet 4 problems, you may find useful
+import HiddenAlgorithms (determiniseNFA, minimiseDFA)
+-- one more module is required for testing purposes
+import Hidden
+-- TODO: Complete this function
+slice :: DFA -> DFA
+slice dfa
+  = undefined
